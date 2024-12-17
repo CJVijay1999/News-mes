6c01e22541dacee9ebcb0fdaa2854d2527fc87fa
Vijay M N <vijaycj05@yahoo.com>
2024-12-17 23:50:14 +0530

    News app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+
+const App = () => {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=2250f3def609d4a1923ad0ba3dc5aaf2`
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
